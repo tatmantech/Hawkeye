6edb2725485291e7c3d4cf3543cb9ab609561228
J E Tatman <47076712+tatmantech@users.noreply.github.com>
2019-03-01 04:04:40 -0600

    log format optimizations

--- server/utils/log.py
+++ server/utils/log.py
@@ -3,4 +3,4 @@ import os
 import syslog
 FORMAT = '%(asctime)s [%(name)s] [%(levelname)s] [%(pathname)s:%(lineno)s %(funcName)s] %(message)s'
 logging.basicConfig(filename='Hawkeye.log', level=logging.INFO,format=FORMAT)
-logger = logging.getLogger()
+logger = logging.getLogger() 
